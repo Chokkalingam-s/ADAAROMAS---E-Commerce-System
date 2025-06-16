8eaff8a9f8bab3841b7b4cf685f02ce129c39eb5
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2025-06-17 00:18:42 +0530

    Product card responsive ui

--- product-card.php
+++ product-card.php
@@ -1,4 +1,4 @@
-<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="col-6 col-sm-6 col-md-4 col-lg-3">
   <div class="product-card shadow position-relative overflow-hidden rounded">
     
     <!-- Image Container with badge and hover button -->
